acc2191ca6b4174938cce953274d20745f31bfae
geraldineclemente56-maker <geraldineclemente56@gmail.com>
2026-04-20 13:45:58 -0300

    fix push

---
+++ fundamentos-basicos/src/main/kotlin/edu/etec/ds/fundamentos/Ejercicio2.kt
@@ -0,0 +1,49 @@
+package edu.etec.ds.fundamentos
+
+fun obtenerTipoEntero(): String {
+    return "Int"
+}
+
+fun obtenerTipoDouble(): String {
+    return "Double"
+}
+
+fun obtenerTipoBoolean(): String {
+    return "Boolean"
+}
+
+fun obtenerTipoString(): String {
+    return "String"
+}
+
+fun obtenerTipoChar(): String {
+    return "Char"
+}
+
+fun obtenerTipoLista(): String {
+    return "List"
+}
+
+fun obtenerTipoMap(): String {
+    return "Map"
+}
+
+fun obtenerTipoArray(): String {
+    return "Array"
+}
+
+fun obtenerTipoNulo(): String {
+    return "Null"
+}
+
+fun obtenerTipoNullable(): String {
+    return "Nullable"
+}
+
+fun obtenerTipoUnit(): String {
+    return "Unit"
+}
+
+fun obtenerTipoNothing(): String {
+    return "Nothing"
+}
