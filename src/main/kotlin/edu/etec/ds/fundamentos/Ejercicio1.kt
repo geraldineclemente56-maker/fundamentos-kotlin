401f2be662dc130d6a2004068628a3774598fd03
geraldineclemente56-maker <geraldineclemente56@gmail.com>
2026-04-20 13:43:09 -0300

    re-creacion repo con ejercicios hechos

---
+++ src/main/kotlin/edu/etec/ds/fundamentos/Ejercicio1.kt
@@ -0,0 +1,61 @@
+package edu.etec.ds.fundamentos
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+fun suma(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun resta(a: Int, b: Int): Int {
+    return a - b
+
+}
+
+fun multiplicacion(a: Int, b: Int): Int {
+
+    return a * b
+}
+
+fun division(a: Int, b: Int): Int {
+
+    return a / b
+}
+
+fun modulo(a: Int, b: Int): Int {
+    return a % b
+
+}
+
+fun esMayorQue(a: Int, b: Int): Boolean {
+
+    return a > b
+}
+
+fun esMenorQue(a: Int, b: Int): Boolean {
+    return a < b
+}
+
+fun sonIguales(a: Int, b: Int): Boolean {
+
+    return a == b
+}
+
+fun esPar(numero: Int): Boolean {
+    return numero % 2 == 0
+}
+
+fun esImpar(numero: Int): Boolean {
+    return numero % 2 == 1
+}
+
+fun valorAbsoluto(numero: Int): Int {
+    return abs(n = numero)
+}
+
+fun maximo(a: Int, b: Int): Int {
+    return max(a,b)
+}
+
+fun minimo(a: Int, b: Int): Int {
+    return min(a,b)
+}
